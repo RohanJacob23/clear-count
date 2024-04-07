b683d698534e805fc379afad8c6b2c2b3ea84d00
RohanJacob23 <rohankoshyjacob@gmail.com>
2024-04-07 22:59:49 +0530

    add new dot background to the home page

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,52 +1,48 @@
 import Reveal from "../animations/home/Reveal";
 import RevealButton from "../animations/home/RevealButton";
+import { TypewriterEffect } from "../animations/home/TypewriterEffect";
 import { Button } from "../ui/button";
 import Link from "next/link";
-import { SparklesCore } from "../ui/Sparkles";
 
 export default function Hero() {
   return (
-    <section className="min-h-screen flex flex-col justify-center">
-      <div className="text-center space-y-4 max-w-3xl relative z-50">
-        <Reveal>
-          <h6 className="lead">Take control of your finances</h6>
-        </Reveal>
-        <Reveal delay={0.2}>
-          <h1 className="max-w-3xl mx-auto">
-            Effortless expense tracking for a clear financial future with
-            <br />
-            <span className="text-4xl bg-gradient-to-r from-primary to-foreground bg-clip-text text-transparent">
-              Clear Count
-            </span>
-          </h1>
-        </Reveal>
+    <section>
+      <div className="min-h-screen flex flex-col justify-center">
+        <div className="text-center space-y-4 max-w-3xl relative z-50">
+          <Reveal>
+            <h6 className="lead">Take control of your finances</h6>
+          </Reveal>
+          <Reveal delay={0.2}>
+            <h1 className="max-w-3xl mx-auto">
+              Effortless expense tracking for a clear financial future with
+            </h1>
+            <TypewriterEffect
+              cursorClassName="bg-primary"
+              delay={1}
+              words={[{ text: "Clear" }, { text: "Count" }]}
+            />
+          </Reveal>
 
-        <Reveal delay={0.4}>
-          <p className="text-muted-foreground">
-            Track your expenses effortlessly, create budgets, and reach your
-            financial goals with ease.
-          </p>
-        </Reveal>
+          <Reveal delay={0.4}>
+            <p className="text-muted-foreground">
+              Track your expenses effortlessly, create budgets, and reach your
+              financial goals with ease.
+            </p>
+          </Reveal>
+        </div>
+        <div className="relative z-50">
+          <RevealButton>
+            <Button asChild className="w-full">
+              <Link href="/dashboard">Get started</Link>
+            </Button>
+            <Button asChild variant="outline" className="w-full">
+              <Link href="/login">Login</Link>
+            </Button>
+          </RevealButton>
+        </div>
       </div>
-      <div className="relative z-50">
-        <RevealButton>
-          <Button asChild className="w-full">
-            <Link href="/dashboard">Get started</Link>
-          </Button>
-          <Button asChild variant="outline" className="w-full">
-            <Link href="/login">Login</Link>
-          </Button>
-        </RevealButton>
-      </div>
-      <div className="absolute inset-0">
-        <SparklesCore
-          id="tsparticlesfullpage"
-          background="transparent"
-          minSize={0.6}
-          maxSize={1.4}
-          particleDensity={100}
-          className="w-full h-full"
-        />
+      <div className="absolute size-full inset-0 dark:bg-dot-white/[0.2] bg-dot-black/[0.2]">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_50%,black)]"></div>
       </div>
     </section>
   );
